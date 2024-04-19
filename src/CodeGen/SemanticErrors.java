6ff33593f57866af4fff8fb1ceb0cdfd7e5c11ed
alex <a75511@ualg.pt>
2024-04-19 10:11:37 +0100

    Mais testes

--- src/CodeGen/SemanticErrors.java
+++ src/CodeGen/SemanticErrors.java
@@ -17,7 +17,6 @@ public class SemanticErrors {
         return this.numErr;
     }
 
-    //Acho que dá para fazer assim um erro genérico ao invés de estar a especificar
     public void GenericErr(SolParser.InstContext ctx) {
         //line 2:20 error: operator - is invalid between bool and int
         System.out.println("line "+ctx.start.getLine() + " error: " + ctx.getChild(1) + " is invalid between " + values.get(ctx.getChild(0)) + " and " + values.get(ctx.getChild(2)));
@@ -26,37 +25,6 @@ public class SemanticErrors {
 
     public void unaryOpErr(SolParser.UnaryContext ctx) {
         System.out.println("line "+ctx.start.getLine()+" error: "+ ctx.op.getText() + " is invalid for type " + values.get(ctx.inst()));
-        numErr++;
-    }
-
-    public void binaryOrOpErr(SolParser.OrContext ctx) {
-        System.out.println("line "+ctx.start.getLine()+" error: invalid types for operation "+ ctx.op.getText());
-        numErr++;
-    }
-
-    public void addSubOpErr(SolParser.AddSubContext ctx) {
-        //line 2:20 error: operator - is invalid between bool and int
-        System.out.println("line "+ctx.start.getLine()+" error: " + ctx.op.getText() + " is invalid between " + values.get(ctx.inst(0)) + " and " + values.get(ctx.inst(1)));
-        numErr++;
-    }
-
-    public void equalOpErr(SolParser.EqualContext ctx) {
-        System.out.println("line "+ctx.start.getLine()+" error: invalid types for operation "+ ctx.op.getText());
-        numErr++;
-    }
-
-    public void andOpErr(SolParser.AndContext ctx) {
-        System.out.println("line "+ctx.start.getLine()+" error: invalid types for operation "+ ctx.op.getText());
-        numErr++;
-    }
-
-    public void relOpErr(SolParser.RelContext ctx) {
-        System.out.println("line "+ctx.start.getLine()+" error: invalid types for operation "+ ctx.op.getText());
-        numErr++;
-    }
-
-    public void multDivOpErr(SolParser.MultDivContext ctx) {
-        System.out.println("line "+ctx.start.getLine()+" error: invalid types for operation "+ ctx.op.getText());
         numErr++;
     }
 
