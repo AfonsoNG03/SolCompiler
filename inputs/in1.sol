430bb7dde3e07bcc0cafde0fcb32307d11b905db
Alexandre Mendes <a75511@ualg.pt>
2024-05-02 19:48:26 +0100

    Tests

---
+++ inputs/in1.sol
@@ -0,0 +1,6 @@
+int x = 10;
+real y = 3.14;
+string s = "hello";
+bool flag = true;
+
+print s;
---
+++ inputs/in3.sol
@@ -0,0 +1,7 @@
+int a = 5;
+
+while a < 10 do
+    begin
+        print a;
+        a = a + 1;
+    end
